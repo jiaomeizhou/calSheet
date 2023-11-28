ce0b7a611f8ce70ad7cdb4a4e31c4d0e5a52c0af
jiaomeizhou <zhoujiaomei@outlook.com>
2023-11-27 17:41:49 -0800

    Fix lock cell which is editing by other user

--- src/Engine/SpreadSheetController.ts
+++ src/Engine/SpreadSheetController.ts
@@ -83,15 +83,10 @@ export class SpreadSheetController {
     // check to see if the user is editing another cell.
     if (this._contributingUsers.has(user)) {
       const userData: ContributingUser = this._contributingUsers.get(user)!;
-      const currentCell: string = userData.cellLabel;
       if (userData.cellLabel !== '' && userData.cellLabel !== cellLabel) {
         if (this._cellsBeingEdited.has(cellLabel) && 
         this._cellsBeingEdited.get(cellLabel) === user) {
           this._cellsBeingEdited.delete(cellLabel); 
-        }
-        else if (this._cellsBeingEdited.has(cellLabel) && 
-        this._cellsBeingEdited.get(cellLabel) !== user) {
-          nextCell = currentCell;
         }
 
       }
@@ -147,7 +142,7 @@ export class SpreadSheetController {
     // if the user is editing a cell then free that one up
     const editingCell: string | undefined = this._contributingUsers.get(user)?.cellLabel;
     if (editingCell) {
-      if (this._cellsBeingEdited.has(editingCell)) {
+      if (this._cellsBeingEdited.has(editingCell) && this._cellsBeingEdited.get(editingCell) === user) {
         this._cellsBeingEdited.delete(editingCell);
       }
     }
